a1a7fa8ad841a53c167f4c1a66873f955fc32d59
marcos <marcoslemeborbafilho@gmail.com>
2020-12-02 15:31:18 -0300

    Pegando o json e printando na tela

--- 01-request.py
+++ 01-request.py
@@ -1,9 +1,18 @@
 import requests
 
+
 def main():
-    response = requests.get("https://api.exchangeratesapi.io/history?start_at=2018-01-01&end_at=2018-09-01 HTTP/1.1")
-    print("Status Code: ", response.status_code)
-    print("Headhes: ", response.headers)
+    response = requests.get(
+        "https://api.exchangeratesapi.io/latest"
+    )
+
+    if response.status_code != 200:
+        print("Status Code: ", response.status_code)
+        raise Exception("There was a error!")
+
+    data = response.json()
+    print("JSON data: ", data)
+
 
 if __name__ == "__main__":
     main()
