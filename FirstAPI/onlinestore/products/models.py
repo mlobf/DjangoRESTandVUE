386a763a05d4e80b65d9f818d2bad89f344154b7
marcos <marcoslemeborbafilho@gmail.com>
2020-12-03 17:10:11 -0300

    migration applied

--- FirstAPI/onlinestore/products/models.py
+++ FirstAPI/onlinestore/products/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Manufacturer(models.Models):
+class Manufacturer(models.Model):
     name = models.CharField(max_length=120)
     location = models.CharField(max_length=120)
     active = models.BooleanField(default=True)
