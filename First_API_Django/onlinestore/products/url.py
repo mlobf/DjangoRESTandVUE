2b4264d5bb780db0f607107e444f12cf8167a064
marcos <marcoslemeborbafilho@gmail.com>
2020-12-03 16:15:21 -0300

    Deleting venv and all PreProject

--- First_API_Django/onlinestore/products/url.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from  .views import ProductDetailsView, ProductListView
-
-urlpatterns = [
-    path("", ProductListView.as_view(), name='product-list'),
-    path("products/<int:pk>/", ProductDetailsView.as_view(), name='product-detail'),
-]
