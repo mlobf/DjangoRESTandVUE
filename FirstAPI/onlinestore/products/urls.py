1be9fb52d01d4c00cd2d368a54e783c69a0fee5e
marcos <marcoslemeborbafilho@gmail.com>
2020-12-07 17:25:31 -0300

    List and Details Api are Ready

--- FirstAPI/onlinestore/products/urls.py
+++ FirstAPI/onlinestore/products/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import product_list #ProductDetailView, ProductListView
+from .views import product_detail, product_list#ProductDetailView, ProductListView
 
 urlpatterns = [
     path("products/", product_list, name="product-list"),
+    path("products/<int:pk>/", product_detail, name="product-detail"),
 ]
 
--- FirstAPI/onlinestore/products/views.py
+++ FirstAPI/onlinestore/products/views.py
@@ -8,29 +8,28 @@ def product_list(request):
     response = JsonResponse(data)
     return response
 
-def product_detail(request,pk):
-    pass
 
+def product_detail(request, pk):
+    try:
+        product = Product.objects.get(pk=pk)
+        data = {
+            "product": {
+                "name": product.name,
+                "manufacturer": product.manufacturer.name,
+                "description": product.description,
+                "photo": product.photo.url,
+                "price": product.price,
+                "shipping_cost": product.shipping_cost,
+                "quantity": product.quantity,
+            }
+        }
 
+        response = JsonResponse(data)
 
+    except Product.DoesNotExist:
 
+        response = JsonResponse(
+            {"error": {"code": 404, "message": "product not found!"}}, status=404
+        )
 
-"""
-from django.shortcuts import render
-
-from django.views.generic.detail import DetailView
-from django.views.generic.list import ListView
-
-from .models import Product, Manufacturer
-
-
-class ProductDetailView(DetailView):
-    model = Product
-    template_name = "products/product_detail.html"
-
-
-class ProductListView(ListView):
-    model = Product
-    template_name = "products/product_list.html"
-
-"""
+    return response
