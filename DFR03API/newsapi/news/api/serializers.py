27395c7bde152cbfb43281a1a0915ed8da1793a5
marcos <marcoslemeborbafilho@gmail.com>
2020-12-08 20:28:25 -0300

    commit

--- DFR03API/newsapi/news/api/serializers.py
+++ DFR03API/newsapi/news/api/serializers.py
@@ -2,8 +2,6 @@ from rest_framework import serializers
 from news.models import Article
 
 
-
-
 class ArticleSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     author = serializers.CharField()
@@ -16,7 +14,7 @@ class ArticleSerializer(serializers.Serializer):
     created_at = serializers.DateTimeField(read_only=True)
     updated_at = serializers.DateTimeField(read_only=True)
 
-    def created(self,validated_data):
+    def created(self, validated_data):
         print(validated_data)
         return Article.objects.create(**validated_data)
 
