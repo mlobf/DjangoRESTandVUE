e2148df55877704d2705d83515314750d71ea813
italia <marcoslemeborbafilho@gmail.com>
2021-01-04 19:49:29 -0300

    Teste do commit

--- 01-request.py
+++ 01-request.py
@@ -2,9 +2,7 @@ import requests
 
 
 def main():
-    response = requests.get(
-        "https://api.exchangeratesapi.io/latest"
-    )
+    response = requests.get("https://api.exchangeratesapi.io/latest")
 
     if response.status_code != 200:
         print("Status Code: ", response.status_code)
