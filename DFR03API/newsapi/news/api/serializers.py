aa6ea56f1112b8c92abf988895018d1f33446bb7
mlobf <marcoslemeborbafilho@gmail.com>
2021-01-09 11:29:17 -0300

    Serializers Lesson

--- DFR03API/newsapi/news/api/serializers.py
+++ DFR03API/newsapi/news/api/serializers.py
@@ -1,6 +1,12 @@
 from rest_framework import serializers
 from news.models import Article
 
+"""
+    How it work's
+        Deserialization
+            1- Parse stream in to python native data type
+"""
+
 
 class ArticleSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
@@ -23,7 +29,7 @@ class ArticleSerializer(serializers.Serializer):
         instance.title = validated_data.get("title", instance.title)
         instance.description = validated_data.get(
             "description", instance.description)
-        instance.body = validated_data.get("body", instance.body)
+        instance.boady = validated_data.get("body", instance.boady)
         instance.location = validated_data.get("location", instance.location)
         instance.publication_data = validated_data.get(
             "publication_date", instance.publication_date
