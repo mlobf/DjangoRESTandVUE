839329089e67f8dfa125c7944ad482a810473579
mlobf <marcoslemeborbafilho@gmail.com>
2021-01-09 13:03:08 -0300

    Api Decorator Part One

--- DFR03API/newsapi/news/api/serializers.py
+++ DFR03API/newsapi/news/api/serializers.py
@@ -20,8 +20,9 @@ class ArticleSerializer(serializers.Serializer):
     created_at = serializers.DateTimeField(read_only=True)
     updated_at = serializers.DateTimeField(read_only=True)
 
-    def created(self, validated_data):
+    def create(self, validated_data):
         print(validated_data)
+
         return Article.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
