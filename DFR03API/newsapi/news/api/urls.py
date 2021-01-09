0189122171ba8f9eb47137d3b3654505408c3998
mlobf <marcoslemeborbafilho@gmail.com>
2021-01-09 15:31:51 -0300

    One Step Before Implementing a Custom Validation

--- DFR03API/newsapi/news/api/urls.py
+++ DFR03API/newsapi/news/api/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
 #from news.api.views import article_detail_api_view, article_list_create_api_view
-from news.api.views import ArticleDetailAPIView , ArticleListCreateAPIView
+from news.api.views import ArticleDetailAPIView, ArticleListCreateAPIView
 
 urlpatterns = [
-    
     path("articles/", ArticleListCreateAPIView.as_view(), name="article-list"),
-
     path("articles/<int:pk>/", ArticleDetailAPIView.as_view(), name="article-detail")
 ]
