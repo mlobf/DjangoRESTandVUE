4010830e71f61421bc3bf30d8cd125868b9ed13e
mlobf <marcoslemeborbafilho@gmail.com>
2021-03-29 21:09:08 -0300

    First Commit

--- DFR03API/newsapi/news/api/serializers.py
+++ DFR03API/newsapi/news/api/serializers.py
@@ -4,6 +4,7 @@ from rest_framework import serializers
 from news.models import Article, Journalist
 
 
+
 class JournalistSerializer(serializers.ModelSerializer):
     class Meta:
         model = Journalist
